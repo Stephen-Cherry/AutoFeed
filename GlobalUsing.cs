6f4ad78b8eadab3fe8fa3c8315566b38afaed268
Stephen Cherry <stephen.m.cherry@outlook.com>
2026-03-29 22:51:20 -0500

    chore: add System and BepInEx.Logging global usings

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,7 +1,9 @@
+global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Reflection;
 global using BepInEx;
 global using BepInEx.Configuration;
+global using BepInEx.Logging;
 global using HarmonyLib;
 global using UnityEngine;
