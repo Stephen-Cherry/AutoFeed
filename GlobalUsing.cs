364016f45b6e604007bd2c214e83146ddd614149
Stephen Cherry <stephen.m.cherry@outlook.com>
2026-03-29 23:53:28 -0500

    chore: wire plugin project to AutoFeed.Core

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -2,6 +2,7 @@ global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Reflection;
+global using AutoFeed.Core;
 global using BepInEx;
 global using BepInEx.Configuration;
 global using BepInEx.Logging;
