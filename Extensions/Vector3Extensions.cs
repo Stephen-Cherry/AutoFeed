709741842e9116ff8b01fccd569d0f6acdcfb191
Stephen Cherry <stephen.m.cherry@outlook.com>
2026-04-01 17:11:48 -0500

    perf: replace Vector3.Distance with sqrMagnitude in container range/sort

--- Extensions/Vector3Extensions.cs
+++ Extensions/Vector3Extensions.cs
@@ -4,18 +4,25 @@ public static class Vector3Extensions
 {
     public static List<Container> GetContainersInRange(this Vector3 center, float radiusRange)
     {
+        var sqrRadius = radiusRange * radiusRange;
         var result = new List<Container>();
         foreach (var piece in Piece.s_allPieces)
         {
             if (piece == null) continue;
             var container = piece.GetComponent<Container>();
             if (container == null) continue;
-            var dist = Vector3.Distance(piece.transform.position, center);
-            if (dist <= radiusRange && IsValidZNetView(container.GetComponent<ZNetView>()) && IsEligibleContainer(container))
+            var sqrDist = (piece.transform.position - center).sqrMagnitude;
+            if (sqrDist <= sqrRadius && IsValidZNetView(container.GetComponent<ZNetView>()) && IsEligibleContainer(container))
                 result.Add(container);
         }
-        result.Sort((a, b) => Vector3.Distance(a.transform.position, center).CompareTo(Vector3.Distance(b.transform.position, center)));
-        return result;
+
+        var sorted = result
+            .Select(c => (container: c, sqrDist: (c.transform.position - center).sqrMagnitude))
+            .OrderBy(x => x.sqrDist)
+            .Select(x => x.container)
+            .ToList();
+
+        return sorted;
     }
 
     private static bool IsValidZNetView(ZNetView? zNetView) =>
